4bc6a65944d6f3fd1997910801c82f78de85dcaf
dengzhu-hub <3052113768@qq.com>
2024-05-14 21:22:44 +0800

    show is loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import QueryProvider from "./react-query/QueryProvider";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+      <ReactQueryDevtools />
+    </QueryProvider>
   </React.StrictMode>
 );
